2fa5abde472c3e897cfa5cfa064ee7b3fce97899
MrTurtle42 <43407146+MrTurtle42@users.noreply.github.com>
2026-03-13 13:01:11 +0100

    Add files via upload

--- adresses.js
+++ adresses.js
@@ -1,3 +1,5 @@
 let streamApiUrl = [ 
-"http://localhost:8083/channel/Axisnew/webrtc", 
+"http://localhost:8083/channel/AxisNew/webrtc", 
+"http://localhost:8083/channel/AxisOld/webrtc", 
+"http://localhost:8083/channel/Sick/webrtc", 
 ];
